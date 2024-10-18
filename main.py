c9380ef7c4d704c7baca48589bdef428944c8cb6
EmreSimseek <emre0816s@gmail.com>
2024-10-18 12:40:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from util import  ShapeDetector, get_color, create_rectangle
+from util import  ShapeDetector, create_rectangle, get_red_mask, get_green_mask
 import  cv2
-import numpy as np
+
 
 cap = cv2.VideoCapture(0)
 top_left, bottom_right = create_rectangle(cap, scale=0.4, aspect_ratio=1.5)
 
 detector = ShapeDetector(min_area=500)
-
+color_name = "red"
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -14,26 +14,32 @@ while True:
         break
 
     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # BGR'yi HSV'ye çevir
-    blurred = cv2.GaussianBlur(hsv_image, (5, 5), 0)
 
     cv2.rectangle(frame, top_left, bottom_right, (255, 255, 255), 2) # Merkez dikdörtgen
 
-    # Renk sınırlarını alalım (örneğin kırmızı için)
-    color_name = "red"  # İsterseniz 'green' yapabilirsiniz
-    color_ranges = get_color(color_name)
+    # Klavye girişlerini kontrol et
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):  # 'q' tuşuna basıldığında çık
+        break
+    elif key == ord('1'):  # 'r' tuşuna basıldığında kırmızı maskeye geç
+        color_name = "red"
+        print("Kırmızı maske seçildi.")
+    elif key == ord('2'):  # 'g' tuşuna basıldığında yeşil maskeye geç
+        color_name = "green"
+        print("Yeşil maske seçildi.")
 
-    # İki kırmızı aralık için maskeleri birleştirme (kırmızı örneğinde)
     if color_name == "red":
-        mask1 = cv2.inRange(blurred, color_ranges[0][0], color_ranges[0][1])
-        mask2 = cv2.inRange(blurred, color_ranges[1][0], color_ranges[1][1])
-        mask = cv2.bitwise_or(mask1, mask2)  # İki maskeyi birleştiriyoruz
+        mask = get_red_mask(hsv_image)
+    elif color_name == "green":
+        mask = get_green_mask(hsv_image)
     else:
-        mask = cv2.inRange(blurred, color_ranges[0], color_ranges[1])
+        print("Geçersiz renk seçimi!")
+        break
 
-
-    processed_frame = detector.process_frame(mask,frame,color_name)
+    processed_frame = detector.process_frame(mask,frame)
 
     # Görüntüleri göster
+    cv2.imshow("Maskfirst", mask)
     cv2.imshow("fm", processed_frame)
 
     # 'q' tuşuna basıldığında döngüden çık
