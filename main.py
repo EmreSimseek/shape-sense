20575839a0e372b5603e9a2c323c9d923bdc2f44
EmreSimseek <emre0816s@gmail.com>
2024-10-18 17:25:56 +0300

    Add center point visualization for detected shapes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from util import  ShapeDetector, create_rectangle, get_red_mask, get_green_mask
+from util import  ShapeDetector, get_red_mask, get_green_mask, create_rectangle
 import  cv2
-
 
 cap = cv2.VideoCapture(0)
 top_left, bottom_right = create_rectangle(cap, scale=0.4, aspect_ratio=1.5)
@@ -15,16 +14,16 @@ while True:
 
     hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # BGR'yi HSV'ye çevir
 
-    cv2.rectangle(frame, top_left, bottom_right, (255, 255, 255), 2) # Merkez dikdörtgen
+    cv2.rectangle(frame, top_left, bottom_right, (255, 255, 255), 3) # Merkez dikdörtgen
 
     # Klavye girişlerini kontrol et
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):  # 'q' tuşuna basıldığında çık
         break
-    elif key == ord('1'):  # 'r' tuşuna basıldığında kırmızı maskeye geç
+    elif key == ord('1'):  # '1' tuşuna basıldığında kırmızı maskeye geç
         color_name = "red"
         print("Kırmızı maske seçildi.")
-    elif key == ord('2'):  # 'g' tuşuna basıldığında yeşil maskeye geç
+    elif key == ord('2'):  # '2' tuşuna basıldığında yeşil maskeye geç
         color_name = "green"
         print("Yeşil maske seçildi.")
 
@@ -42,9 +41,7 @@ while True:
     cv2.imshow("Maskfirst", mask)
     cv2.imshow("fm", processed_frame)
 
-    # 'q' tuşuna basıldığında döngüden çık
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+
 
 
 # Kamera serbest bırak ve pencereleri kapat
